9ed9bf36c2fd8c84c0e0a3e930d8eaa9a9bcbd92
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2019-08-10 19:58:46 -0700

    Add idea list selected

--- submission-form-script.js
+++ submission-form-script.js
@@ -1,6 +1,7 @@
 function alertFields(){
     let ideaName = document.getElementById("idea-name").value;
     let submitterName = document.getElementById("submitter-name").value;
+    let ideaListSelect = document.getElementById("idea-list-select").value;
     let ideaDescription = document.getElementById("idea-description").value;
-    alert("Idea Name: " + ideaName + "\nSubmitter Name: " + submitterName + "\nIdea Description: " + ideaDescription);
+    alert("Idea Name: " + ideaName + "\nSubmitter Name: " + submitterName + "\nIdea List selected: " + ideaListSelect + "\nIdea Description: " + ideaDescription);
 }
