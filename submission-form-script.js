bf355478c04647c3851eec556b75880795420150
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2019-08-10 20:16:31 -0700

    Implement the cancel button

--- submission-form-script.js
+++ submission-form-script.js
@@ -1,7 +1,17 @@
+//Get the filled out values and alert them
 function alertFields(){
+    
     let ideaName = document.getElementById("idea-name").value;
     let submitterName = document.getElementById("submitter-name").value;
     let ideaListSelect = document.getElementById("idea-list-select").value;
     let ideaDescription = document.getElementById("idea-description").value;
     alert("Idea Name: " + ideaName + "\nSubmitter Name: " + submitterName + "\nIdea List selected: " + ideaListSelect + "\nIdea Description: " + ideaDescription);
+}
+
+//Clear all of the fields to fill out
+function clearFields(){
+    document.getElementById("idea-name").value = "";
+    document.getElementById("submitter-name").value = "";
+    document.getElementById("idea-list-select").value = "";
+    document.getElementById("idea-description").value = "";
 }
