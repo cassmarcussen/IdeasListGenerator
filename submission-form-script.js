aad539cf778bcca94e7651ebf5da19a0658ed876
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2019-08-10 19:25:17 -0700

    Alert the field values

--- submission-form-script.js
+++ submission-form-script.js
@@ -1,1 +1,6 @@
-console.log('Setting up');
+function alertFields(){
+    let ideaName = document.getElementById("idea-name").value;
+    let submitterName = document.getElementById("submitter-name").value;
+    let ideaDescription = document.getElementById("idea-description").value;
+    alert("Idea Name: " + ideaName + "\nSubmitter Name: " + submitterName + "\nIdea Description: " + ideaDescription);
+}
